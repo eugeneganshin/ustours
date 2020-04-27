9baef6c2a864cfe5b9bceee72214c8f2ff5f1894
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-04-27 20:15:15 +0300

    mongoose schema

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
-const dotenv = require('dotenv')
-dotenv.config({path: './config.env'})
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const app = require('./app')
+dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-const port = process.env.PORT = 4000 || 3000
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection is successfull');
+  });
 
-app.listen(port, () => console.log('http://localhost:4000/'))
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const app = require('./app');
+
+// eslint-disable-next-line no-multi-assign
+const port = (process.env.PORT = 4000 || 3000);
+
+app.listen(port, () => console.log('http://localhost:4000/'));
