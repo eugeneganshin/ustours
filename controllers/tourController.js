4757c1f3e08a9776d0e795d1ceaa59b26a8af299
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-04-28 21:24:05 +0300

    sorting and field limiter

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,9 @@
 const Tour = require('../models/tourModel');
 
+/**
+ *TODO: write a documentation to specify which features a user can query the API
+ *
+ */
 exports.getAllTours = async (req, res, next) => {
   try {
     // BUILD QUERY
@@ -8,14 +12,27 @@ exports.getAllTours = async (req, res, next) => {
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advanced filtering.
-    // TODO: write a documentation to specify which features
-    // a user can query the API
+    // 1B) Advanced filtering.
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
+    // 2) Sorting.
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // Field limiter.
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
     // EXECUTE QUERY
     const tours = await query;
 
