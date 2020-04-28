057b31ec523d48d4de54827f4f0359360ef61970
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-04-28 23:05:40 +0300

    aggregation pipeline and projecting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,15 +3,20 @@ const tourControl = require('../controllers/tourController');
 
 const router = express.Router();
 
+router
+  .route('/top-5-cheap')
+  .get(tourControl.aliasTopTours, tourControl.getAllTours);
+
+router.route('/tour-stats').get(tourControl.getTourStats);
+router.route('/monthly-plan/:year').get(tourControl.getMonthlyPlan);
+
 // eslint-disable-next-line prettier/prettier
 router
   .route('/')
   .get(tourControl.getAllTours)
   .post(tourControl.createTour);
 
-router
-  .route('/top-5-cheap')
-  .get(tourControl.aliasTopTours, tourControl.getAllTours);
+
 
 router
   .route('/:id')
