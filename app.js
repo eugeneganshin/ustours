dbe3285bb97b367a8db9d25dd3f6f020c9c20395
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-05-05 18:52:14 +0300

    xss, parameter polution, noSQL injection

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,11 +13,44 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middlewares
+// GLOBAL MIDDLEWARE //
+
+// Secure http headers.
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+const limiter = rateLimit({
+  // Limit requests from same ip.
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 1h
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser.
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against noSQL query injection,
+// Data sanitization against XSS,
+// Prevent parameter polution.
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+// Serving static files.
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,17 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOpt = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOpt.secure = true;
+
+  res.cookie('jwt', token, cookieOpt);
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
