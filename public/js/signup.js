b08e26684acb55fe05bfa11b6e2fcd6faf777aea
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-08-05 13:24:36 +0300

    fixed cors

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,28 +1,27 @@
 /* eslint-disable */
-import axios from 'axios'
-import { showAlert } from './alerts'
+import axios from 'axios';
+import { showAlert } from './alerts';
 
-export const signup = async  (name,email, password, passwordConfirm) => {
+export const signup = async (name, email, password, passwordConfirm) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:4000/api/v1/users/signup',
+      url: '/api/v1/users/signup',
       data: {
         name,
         email,
         password,
-        passwordConfirm
-      }
-    })
+        passwordConfirm,
+      },
+    });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Signed up successfully')
-      window.setTimeout(()=>{
-        location.assign('/')
-      }, 1000)
+      showAlert('success', 'Signed up successfully');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1000);
     }
-
   } catch (error) {
-    showAlert('error', error.response.data.message)
+    showAlert('error', error.response.data.message);
   }
-}
+};
